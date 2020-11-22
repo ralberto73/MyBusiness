950b01ddcfb5afce5a7e62467b824ce6d09d8657
Raul Perez <ralberto73@gmail.com>
2020-11-21 19:34:41 -0500

    First Commit

---
+++ MyBusiness.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyBusiness.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ MyBusiness/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyBusiness.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
