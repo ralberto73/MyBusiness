6531e006d43d8ecd827cb1b763c34832a3768d63
Raul Perez <ralberto73@gmail.com>
2020-11-29 11:25:47 -0500

    Products Index View

--- MyBusiness.Models/Product.cs
+++ MyBusiness.Models/Product.cs
@@ -12,15 +12,20 @@ namespace MyBusiness.Models
         [Key]
         public int ProductId { get; set; }
         [Required]
+        [Display(Name = "Category")]
         public int ProdCategoryId { get; set; }
 
         [Required]
+        [Display(Name = "Name")]
         public string ProductName { get; set; }
-
+        [Display(Name = "Created By")]
         public string CreatedBy { get; set; }
+        
+        [Display(Name = "Updated By")]
         public string UpdatedBy { get; set; }
+        [Display(Name = "Creation Date")]
         public DateTime CreationDate { get; set; }
-
+        [Display(Name = "Last Update")]
         public DateTime LastUpdateDate { get; set; }
 
     }
